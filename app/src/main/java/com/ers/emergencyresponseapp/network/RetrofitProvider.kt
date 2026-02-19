1950cc30d353e56f67c12f735540dc11a5fd4fdb
Lloyd <lloydsamonte7@gmail.com>
2026-02-20 00:01:45 +0800

    other things

--- app/src/main/java/com/ers/emergencyresponseapp/network/RetrofitProvider.kt
+++ app/src/main/java/com/ers/emergencyresponseapp/network/RetrofitProvider.kt
@@ -1,6 +1,7 @@
 package com.ers.emergencyresponseapp.network
 
 import android.util.Log
+import com.ers.emergencyresponseapp.BuildConfig
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -10,60 +11,35 @@ object RetrofitProvider {
     private const val BASE_URL = "https://hytographicallly-nondistorted-aurelia.ngrok-free.dev/"
 
     private val httpLoggingInterceptor: HttpLoggingInterceptor by lazy {
-        HttpLoggingInterceptor { message ->
-            Log.d("RetrofitHttp", message)
-        }.apply {
-            level = HttpLoggingInterceptor.Level.BODY
-        }
-    }
-
-    private val okHttpClient: OkHttpClient by lazy {
-        OkHttpClient.Builder()
-            .addInterceptor(httpLoggingInterceptor)
-            .build()
-    }
-
-    private val loggingClient: OkHttpClient by lazy {
-        OkHttpClient.Builder()
-            .addInterceptor { chain ->
-                val request = chain.request()
-                Log.d("RetrofitProvider", "Request URL: ${request.method} ${request.url}")
-                chain.proceed(request)
+            HttpLoggingInterceptor { message ->
+                Log.d("RetrofitHttp", message)
+            }.apply {
+                level = if (BuildConfig.DEBUG) {
+                    HttpLoggingInterceptor.Level.BODY
+                } else {
+                    HttpLoggingInterceptor.Level.NONE
+                }
             }
-            .build()
-    }
-
-    private val httpLoggingInterceptor: HttpLoggingInterceptor by lazy {
-        HttpLoggingInterceptor { message ->
-            Log.d("RetrofitHttp", message)
-        }.apply {
-            level = HttpLoggingInterceptor.Level.BODY
         }
-    }
 
-    private val okHttpClient: OkHttpClient by lazy {
-        OkHttpClient.Builder()
-            .addInterceptor { chain ->
-                val request = chain.request()
-                Log.d("RetrofitProvider", "Request URL: ${request.method} ${request.url}")
-                chain.proceed(request)
-            }
-            .addInterceptor(httpLoggingInterceptor)
-            .build()
-    }
+        private val okHttpClient: OkHttpClient by lazy {
+            OkHttpClient.Builder()
+                .addInterceptor { chain ->
+                    val request = chain.request()
+                    Log.d("RetrofitProvider", "Request URL: ${request.method} ${request.url}")
+                    chain.proceed(request)
+                }
+                .addInterceptor(httpLoggingInterceptor)
+                .build()
+        }
 
-    private val retrofit: Retrofit by lazy {
-        Retrofit.Builder()
-            .baseUrl(BASE_URL)
-            .client(okHttpClient)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-    }
+        private val retrofit: Retrofit by lazy {
+            Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .client(okHttpClient)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+        }
 
-    val authApi: AuthApi by lazy {
-        retrofit.create(AuthApi::class.java)
+        val authApi: AuthApi by lazy { retrofit.create(AuthApi::class.java) }
     }
-
-    val authApi: AuthApi by lazy { retrofit.create(AuthApi::class.java) }
-    val apiService: ApiService by lazy { retrofit.create(ApiService::class.java) }
-}
